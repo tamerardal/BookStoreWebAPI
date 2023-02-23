388c979a132ba71ed01c3f9f1a7ef7d86755e89b
Tamer <47778933+tamerardal@users.noreply.github.com>
2023-02-23 14:06:24 +0300

    data generator oluşturuldu.

---
+++ BookStoreApi/DB/DataGenerator.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+
+public class DataGenerator
+{
+	public static void Initialize(IServiceProvider serviceProvider)
+	{
+		using (var context = new BookStoreDbContext(serviceProvider.GetRequiredService<DbContextOptions<BookStoreDbContext>>()))
+		{
+			if (context.Books.Any())
+			{
+				return ;
+			}
+			
+			context.Books.AddRange(
+				new Book
+				{
+					Id = 1,
+					Title = "Book of 5 Rings",
+					Author = "Miyamoto Musashi",
+					GenreId = 5, //	Philosophy
+					PageCount = 128,
+					PublishDate = new DateTime(1645, 1, 1),
+				},
+				new Book
+				{
+					Id = 2,
+					Title = "Meditations",
+					Author = "Marcus Aurelius",
+					GenreId = 5, //	Philosophy
+					PageCount = 112,
+					PublishDate = new DateTime(54, 1, 1),
+				},
+				new Book
+				{
+					Id = 3,
+					Title = "Dune",
+					Author = "Frank Herbert",
+					GenreId = 2, //	Science-Fiction
+					PageCount = 879,
+					PublishDate = new DateTime(2001, 1, 1),
+				}
+			);
+			
+			context.SaveChanges();
+		}
+	}
+}
