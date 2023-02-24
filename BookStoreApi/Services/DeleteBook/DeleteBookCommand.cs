b61a6decb7b2430e6e0513ff1c7fa0225adb60f4
Tamer <47778933+tamerardal@users.noreply.github.com>
2023-02-24 15:28:33 +0300

    delete command.

---
+++ BookStoreApi/Services/DeleteBook/DeleteBookCommand.cs
@@ -0,0 +1,21 @@
+public class DeleteBookCommand
+{
+	private readonly BookStoreDbContext _dbContext;
+	public int BookId { get; set; }
+	
+	public DeleteBookCommand(BookStoreDbContext dbContext)
+	{
+		_dbContext = dbContext;
+	}
+	public void Handle()
+	{
+		var book = _dbContext.Books.SingleOrDefault(book => book.Id == BookId);
+		
+		if (book is null)
+			throw new InvalidOperationException("Book doesn't exist!");
+
+		
+		_dbContext.Books.Remove(book);
+		_dbContext.SaveChanges();
+	}
+}
