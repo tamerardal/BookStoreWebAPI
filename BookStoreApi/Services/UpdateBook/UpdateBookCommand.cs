0138c0118bc6ec64424f12c67e5952f1fe60fda9
Tamer <47778933+tamerardal@users.noreply.github.com>
2023-02-24 15:28:52 +0300

    update command.

--- BookStoreApi/Services/UpdateBook/UpdateBookCommand.cs
+++ BookStoreApi/Services/UpdateBook/UpdateBookCommand.cs
@@ -1,17 +1,37 @@
 public class UpdateBookCommand
 {
 	private readonly BookStoreDbContext _dbContext;
+	public int BookId { get; set; }
+	public UpdateBookModel Model;
 	public UpdateBookCommand(BookStoreDbContext dbContext)
 	{
 		_dbContext = dbContext;
+	}
+	public void Handle()
+	{
+		var book = _dbContext.Books.SingleOrDefault(book => book.Id == BookId);
+		
+		if (book is null)
+		{
+			throw new InvalidOperationException("Book doesn't exist!");
+		}
+		
+		book.GenreId = Model.GenreId != default ? Model.GenreId : book.GenreId;
+		book.Author = Model.Author != default ? Model.Author : book.Author;
+		book.PageCount = Model.PageCount != default ? Model.PageCount : book.PageCount;
+		book.Title = Model.Title != default ? Model.Title : book.Title;
+		book.PublishDate = Model.PublishDate != default ? Model.PublishDate : book.PublishDate;
+			
+		_dbContext.SaveChanges();
+
 	}
 }
 
 public class UpdateBookModel
 {
 	public string? Title { get; set; }
-	public string? Genre { get; set; }
+	public int GenreId { get; set; }
 	public string? Author { get; set; }
 	public int PageCount { get; set; }
-	public int PublishDate { get; set; }
+	public DateTime PublishDate { get; set; }
 }
