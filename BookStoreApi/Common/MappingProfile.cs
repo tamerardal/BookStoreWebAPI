a7f3da5e8c335173db616f933ece947da57a57ae
Tamer <47778933+tamerardal@users.noreply.github.com>
2023-02-26 00:29:33 +0300

    genre entity'sinin mapleri oluşturuldu.

--- BookStoreApi/Common/MappingProfile.cs
+++ BookStoreApi/Common/MappingProfile.cs
@@ -3,10 +3,13 @@ using BookStoreApi.Applications.BookOperations.Commands.CreateBook;
 using BookStoreApi.Applications.BookOperations.Commands.GetBookDetail;
 using BookStoreApi.Applications.BookOperations.Commands.GetBooks;
 using BookStoreApi.Applications.BookOperations.Commands.UpdateBook;
+using static CreateGenreCommand;
+using static GetGenreDetailQuery;
+using static GetGenresQuery;
 
 namespace BookStoreApi.Common
 {
-    public class MappingProfile : Profile
+	public class MappingProfile : Profile
 	{
 		public MappingProfile()
 		{
@@ -14,6 +17,11 @@ namespace BookStoreApi.Common
 			CreateMap<Book, BookDetailViewModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => ((GenreEnum)src.GenreId).ToString()));
 			CreateMap<CreateBookCommand.CreateBookViewModel, Book>();
 			CreateMap<UpdateBookViewModel, Book>();
+			
+			CreateMap<Genre, GenreViewModel>();
+			CreateMap<Genre, GenreDetailViewModel>();
+			CreateMap<CreateGenreViewModel, Genre>();
+			CreateMap<UpdateGenreViewModel, Genre>();
 		}
 	}
 }
