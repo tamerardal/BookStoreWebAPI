8af90da371896d33fd9e60b2232b150a28c63080
Tamer <47778933+tamerardal@users.noreply.github.com>
2023-02-26 13:50:26 +0300

    author contoller eklendi.

--- BookStoreApi/Controllers/AuthorController.cs
+++ BookStoreApi/Controllers/AuthorController.cs
@@ -1,5 +1,7 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using static CreateAuthorCommand;
+using static UpdateAuthorCommand;
 
 [ApiController]
 [Route("[controller]s")]
@@ -28,5 +30,44 @@ public class AuthorController : ControllerBase
 		query.AuthorId = id;
 		
 		return Ok(query.Handle());
+	}
+	
+	[HttpPost]
+	public IActionResult AddAuthor([FromBody] CreateAuthorViewModel newAuthor)
+	{
+		CreateAuthorCommand command = new CreateAuthorCommand(_context, _mapper);
+
+		command.Model = newAuthor;
+		
+		//VALIDATIONS
+		
+		command.Handle();
+		
+		return Ok();
+	}
+	
+	[HttpPut("{id}")]
+	public IActionResult UpdateAuthor(int id, [FromBody] UpdateAuthorViewModel updatedAuthor)
+	{
+		UpdateAuthorCommand command = new UpdateAuthorCommand(_context, _mapper);
+		
+		command.AuthorId = id;
+		command.Model = updatedAuthor;
+		
+		command.Handle();
+		
+		return Ok();
+	}
+	
+	[HttpDelete("{id}")]
+	public IActionResult RemoveAuthor(int id)
+	{
+		DeleteAuthorCommand command = new DeleteAuthorCommand(_context);
+		
+		command.AuthorId = id;
+		//VALIDATIONS
+		command.Handle();
+		
+		return Ok();
 	}
 }
