64ee4390d8f7bf2fea43da54816fa3995945df95
Tamer <47778933+tamerardal@users.noreply.github.com>
2023-02-26 13:50:15 +0300

    düzenlendi.

--- BookStoreApi/Applications/BookOperations/Queries/GetBooks/GetBookQuery.cs
+++ BookStoreApi/Applications/BookOperations/Queries/GetBooks/GetBookQuery.cs
@@ -25,7 +25,6 @@ namespace BookStoreApi.Applications.BookOperations.Commands.GetBooks
 	public class BooksViewModel
 	{
 		public string Title { get; set; }
-		public string Author { get; set; }
 		public int PageCount { get; set; }
 		public string PublishDate { get; set; }
 		public string Genre { get; set; }
