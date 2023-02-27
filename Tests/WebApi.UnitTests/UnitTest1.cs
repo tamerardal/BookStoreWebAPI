b493ae0dc547632aa48f4411f8f730ae4e7e2fae
Tamer <47778933+tamerardal@users.noreply.github.com>
2023-02-27 13:07:11 +0300

    build.

--- Tests/WebApi.UnitTests/UnitTest1.cs
+++ Tests/WebApi.UnitTests/UnitTest1.cs
@@ -2,9 +2,13 @@ namespace WebApi.UnitTests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
-    {
+	[Fact]
+	public void Test1()
+	{
 
-    }
+	}
+	public void Test2()
+	{
+		
+	}
 }
