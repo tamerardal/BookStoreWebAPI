6aa5d5a67ee02e98955f3c358023509e696d3a5c
Tamer <47778933+tamerardal@users.noreply.github.com>
2023-02-28 15:05:31 +0300

    createbookcommand ve validator testleri yazıldı.

--- BookStoreApi/Applications/BookOperations/Commands/CreateBook/CreateBookCommandValidator.cs
+++ BookStoreApi/Applications/BookOperations/Commands/CreateBook/CreateBookCommandValidator.cs
@@ -6,8 +6,8 @@ namespace BookStoreApi.Applications.BookOperations.Commands.CreateBook
     {
         public CreateBookCommandValidator()
         {
-            RuleFor(command => command.Model.GenreId).GreaterThan(0);
-            RuleFor(command => command.Model.PageCount).GreaterThan(0);
+            RuleFor(command => command.Model.GenreId).GreaterThan(0).LessThan(20);
+            RuleFor(command => command.Model.PageCount).GreaterThan(0).LessThan(5000);
             RuleFor(command => command.Model.PublishDate.Date).NotEmpty().LessThan(DateTime.Now.Date);
             RuleFor(command => command.Model.Title).NotEmpty().MinimumLength(3);
         }
--- Tests/WebApi.UnitTests/Applications/BookOperations/Commands/CreateBookCommand/CreateBookCommandTests.cs
+++ Tests/WebApi.UnitTests/Applications/BookOperations/Commands/CreateBookCommand/CreateBookCommandTests.cs
@@ -7,6 +7,7 @@ public class CreateBookCommandTests : IClassFixture<CommonTestFixture>
 {
 	private readonly BookStoreDbContext _context;
 	private readonly IMapper _mapper;
+	
 
 	public CreateBookCommandTests(CommonTestFixture testFixture)
 	{
@@ -29,4 +30,27 @@ public class CreateBookCommandTests : IClassFixture<CommonTestFixture>
 	
 		FluentActions.Invoking(() => command.Handle()).Should().Throw<InvalidOperationException>().And.Message.Should().Be("Book is already added!");
 	}
+	[Fact]
+	public void WhenValidInputsAreGiven_Book_ShouldBeCreated()
+	{
+		//arrenge
+		CreateBookCommand command = new CreateBookCommand(_context, _mapper);
+		CreateBookCommand.CreateBookViewModel model = new CreateBookCommand.CreateBookViewModel() {Title = "Hobbits", PageCount = 1000, PublishDate = DateTime.Now.Date.AddYears(-10), GenreId = 1, AuthorId = 2};
+		
+		command.Model = model;
+		
+		//act
+		FluentActions.Invoking(() => command.Handle()).Invoke();
+
+		//assert
+		var book = _context.Books.SingleOrDefault(book => book.Title == model.Title);
+		
+		book.Should().NotBeNull();
+		book.Title.Should().Be(model.Title);
+		book.PageCount.Should().Be(model.PageCount);
+		book.PublishDate.Should().Be(model.PublishDate);
+		book.GenreId.Should().Be(model.GenreId);
+		book.AuthorId.Should().Be(model.AuthorId);
+	}
+	
 }
