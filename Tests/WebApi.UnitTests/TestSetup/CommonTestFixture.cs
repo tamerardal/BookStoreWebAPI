02113d798b2aea22f5f9b3a959eba616060d76a0
Tamer <47778933+tamerardal@users.noreply.github.com>
2023-03-01 17:45:28 +0300

    author test base.

--- Tests/WebApi.UnitTests/TestSetup/CommonTestFixture.cs
+++ Tests/WebApi.UnitTests/TestSetup/CommonTestFixture.cs
@@ -9,13 +9,14 @@ public class CommonTestFixture
 	public CommonTestFixture()
 	{
 		
-        var options = new DbContextOptionsBuilder<BookStoreDbContext>().UseInMemoryDatabase("BookStoreTestDB").Options;
+		var options = new DbContextOptionsBuilder<BookStoreDbContext>().UseInMemoryDatabase("BookStoreTestDB").Options;
 		Context = new BookStoreDbContext(options);
 		
 		Context.Database.EnsureCreated();
 		
 		Context.AddBooks();
 		Context.AddGenres();
+		Context.AddAuthors();
 		Context.SaveChanges();
 		
 		Mapper = new MapperConfiguration(cfg => { cfg.AddProfile<MappingProfile>(); }).CreateMapper();
