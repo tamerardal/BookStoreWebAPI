55c971e10a6f95439e0b2f58957ffd818503b8c4
Tamer <47778933+tamerardal@users.noreply.github.com>
2023-03-02 20:58:23 +0300

    db oluşturuldu&paketler yükelndi.

---
+++ MovieStoreApi/Common/MappingProfile.cs
@@ -0,0 +1,25 @@
+using AutoMapper;
+using static GetMoviesQuery;
+
+public class MappingProfile : Profile
+{
+	public MappingProfile()
+	{
+		CreateMap<Movie, MoviesViewModel>()
+		.ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.Name))
+		.ForMember(dest => dest.Director, opt => opt.MapFrom(src => src.Director.Name + " " + src.Director.Surname))
+		.ForMember(dest => dest.Performers, opt => opt.MapFrom(src => returnActors(src.Performers)));
+		
+		
+		
+	}
+	public List<string> returnActors(List<Performer> actors)
+	{
+	  List<string> performerNames = new List<string>();
+	  foreach(Performer performer in actors)
+	  {
+		performerNames.Add(performer.Name + " " + performer.Surname);
+	  }
+	  return performerNames;
+	}
+}
---
+++ MovieStoreApi/DB/IMovieStoreDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+public interface IMovieStoreDbContext
+{
+    public DbSet<Movie> Movies {get; set;}
+	public DbSet<Director> Directors { get; set; }
+	public DbSet<Performer> Performers { get; set; }
+	public DbSet<Genre> Genres { get; set; }
+	public DbSet<Customer> Customers { get; set; }
+	int SaveChanges();
+}
